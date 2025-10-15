7373cc9f68260cb1e11e613a84a7aa2c819b2e5c
adinsmore <adinsmore@bsd220.org>
2025-10-15 10:49:06 -0500

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Alissa Dinsmore on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
